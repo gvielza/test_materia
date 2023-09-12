77f3e929e2c2d2fe7a6050a57826defd26fc259c
geo <voltagnelly@gmail.com>
2023-09-12 12:01:25 -0300

    agregado metodo suma

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@ def mi_funcion():
    print("mi primer Hola con Git "+"\n"+"Usando funciones")
 
 mi_funcion()
+
+def suma(a,b):
+   return print("La suma es  "+str(a+b))
+
+suma(int(input("ingrese el primer numero: ")),b=int(input("ingrese el segundo numero: ")))
