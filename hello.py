45d35e9288c1bf27489d4b3088f4b98e2dea3376
geo <voltagnelly@gmail.com>
2023-09-12 15:09:16 -0300

    corregido error de la llamada a la funcion resta

--- hello.py
+++ hello.py
@@ -10,8 +10,7 @@ def suma(a,b):
 suma(int(input("ingrese el primer numero: ")),int(input("ingrese el segundo numero: ")))
 
 #funcion para restar
-def resta(a,b)
-   return print("La reesta es  "+str(a-b))
-suma(int(input("ingrese el primer numero: ")),int(input("ingrese el segundo numero: ")))
-
+def resta(a,b):
+   return print("La resta es  "+str(a-b))
+resta(int(input("ingrese el primer numero: ")),int(input("ingrese el segundo numero: ")))
 
