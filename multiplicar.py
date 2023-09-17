96c7006ba2439944f5eb27c9abaf91c6418410e9
MartinaAsad <lasupermartu15@gmail.com>
2023-09-17 15:56:22 -0300

    funcion para multiplicar lista

---
+++ multiplicar.py
@@ -0,0 +1,10 @@
+n1=int(input("Ingrese un numero: "))
+n2=int(input("Ingrese otro numero: "))
+
+def multiplicar(nro, nro2):
+    total=nro*nro2
+    print("el total de la multiplicacion es: ",total)
+
+multiplicar(n1,n2)
+
+
