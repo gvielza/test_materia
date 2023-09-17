b652f01adbdf5e0657f08edb404771919091d09d
MartinaAsad <lasupermartu15@gmail.com>
2023-09-17 15:58:51 -0300

    funcion dividir temrinada

---
+++ dividir.py
@@ -0,0 +1,8 @@
+n1=float(input("Ingrese un numero: "))
+n2=float(input("Ingrese otro numero: "))
+
+def dividir(nro, nro2):
+    total=nro/nro2
+    print("el total de la division es: ",round(total,2))
+
+dividir(n1,n2)
