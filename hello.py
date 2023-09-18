fed65c841882b55730a50100a7ae3f1d2767effb
facupreiss <115724589+facupreiss@users.noreply.github.com>
2023-09-17 23:49:04 -0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,26 @@
+#funcion principal
+def mi_funcion():
+   print("mi primer Hola con Git "+"\n"+"Usando funciones")
+#llamado de la funcion
+mi_funcion()
+
+def suma(a,b):
+   return print("La suma es  "+str(a+b))
+
+suma(int(input("ingrese el primer numero: ")),int(input("ingrese el segundo numero: ")))
+
+#funcion para restar
+def resta(a,b):
+   return print("La resta es  "+str(a-b))
+resta(int(input("ingrese el primer numero: ")),int(input("ingrese el segundo numero: ")))
+
+#funcion para dividir
+def dividir(a,b):
+   return print("La division es  "+str(a/b))
+dividir(int(input("ingrese el primer numero: ")),int(input("ingrese el segundo numero: ")))
+
+#funcion para multiplicar
+def multi(a,b):
+   return print("La multiplicacion es  "+str(a*b))
+multi(int(input("ingrese el primer numero: ")),int(input("ingrese el segundo numero: ")))
+
